459500a4ac4e9cf3f0fe2888c4edbc4bc3eb6bec
yashiro4030 <Administrateur@FDELL-C3JTBB2.hckm.ma>
2021-09-20 17:22:12 +0100

    first save

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'app',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+
+        return $this->render('blog/home.html.twig') ;
+    }
+
+
+}
